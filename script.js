f3a5f75566e1bedc5c517a2eac7c8ccd02103c65
waalidait <snwalid08@gmail.com>
2025-09-16 13:19:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let nombre1 = 10;
-let nombre2 = 5;
-let somme = nombre1 + nombre2;
-console.log("La somme est : " + somme);
+ function addition(a,b){
+    return a + b;
+ }
+ let somme = addition(8,5);
+ console.log("la somme est =",+ somme);
